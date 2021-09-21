7b3d270dcf3a952371f2162a83453fbc546f20be
Zion <zionman@verizon.net>
2021-09-21 15:47:41 -0500

    Change datasource to be managed by spring
    https://mkyong.com/spring-boot/spring-boot-spring-data-jpa-mysql-example/

--- src/main/java/com/cts/capstone/AppConfiguration.java
+++ src/main/java/com/cts/capstone/AppConfiguration.java
@@ -1,42 +1,34 @@
 package com.cts.capstone;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @Configuration
 //@EnableTransactionManagement
 @ComponentScan("com.cts")
 public class AppConfiguration {
-	@Bean
-	public JdbcTemplate jdbcTemplate() {
-		return new JdbcTemplate(mysqlDataSource());
-	}
+//	@Bean
+//	public JdbcTemplate jdbcTemplate() {
+//		return new JdbcTemplate(mysqlDataSource());
+//	}
 
-	@Bean
-	public DataSource mysqlDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		InputStream input = classLoader.getResourceAsStream("connection.properties");
-		Properties properties = new Properties();
-		try {
-			properties.load(input);
-			dataSource.setDriverClassName(properties.getProperty("driver"));
-			dataSource.setUrl(properties.getProperty("url"));
-			dataSource.setUsername(properties.getProperty("username"));
-			dataSource.setPassword(properties.getProperty("password"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return dataSource;
-	}
+//	@Bean
+//	public DataSource mysqlDataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+//		InputStream input = classLoader.getResourceAsStream("connection.properties");
+//		Properties properties = new Properties();
+//		try {
+//			properties.load(input);
+//			dataSource.setDriverClassName(properties.getProperty("driver"));
+//			dataSource.setUrl(properties.getProperty("url"));
+//			dataSource.setUsername(properties.getProperty("username"));
+//			dataSource.setPassword(properties.getProperty("password"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return dataSource;
+//	}
 
 //	@Bean
 //	public PlatformTransactionManager txManager() {
