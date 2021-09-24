f509927e02870e7cfb373292942a79875298eb9f
Zion <zionman@verizon.net>
2021-09-24 15:26:17 -0500

    Removed repositories: unnecessary, and did not want to maintain multiple redundant services

--- src/main/java/com/cts/capstone/repository/OrderDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cts.capstone.repository;
-
-import com.cts.capstone.bean.OrderDetails;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderDetailsRepository extends CrudRepository<OrderDetails, Long> {
-//	public Optional<OrderDetails> findByCustomerId(Long customerId);
-}
