6b04efc068159c9a44dc0533ca7d128784774f15
Zion <zionman@verizon.net>
2021-09-29 12:34:54 -0500

    Removed default date

--- src/main/java/com/cts/capstone/controller/CartController.java
+++ src/main/java/com/cts/capstone/controller/CartController.java
@@ -7,7 +7,6 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -65,9 +64,6 @@ public class CartController {
 	 */
 	@PostMapping(value = "order")
 	public Order createOrder(@RequestBody Order order) {
-		if (order.getOrderDate() == null) {
-			order.setOrderDate(LocalDate.now());
-		}
 		if (!service.createOrder(order)) {
 			return null;
 		}
