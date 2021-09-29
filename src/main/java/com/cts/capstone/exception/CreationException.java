77b2ca8732d592af7393ee29280cb37b21006cb2
Zion <zionman@verizon.net>
2021-09-29 13:39:47 -0500

    Removed unneeded constructors

--- src/main/java/com/cts/capstone/exception/CreationException.java
+++ src/main/java/com/cts/capstone/exception/CreationException.java
@@ -2,14 +2,6 @@ package com.cts.capstone.exception;
 
 public class CreationException extends RuntimeException {
 
-	public CreationException() {
-		super();
-	}
-
-	public CreationException(String type, long id) {
-		this(type, id, null);
-	}
-
 	public CreationException(String type, long id, Throwable cause) {
 		this(type, String.valueOf(id), cause);
 	}
@@ -18,7 +10,4 @@ public class CreationException extends RuntimeException {
 		super("Could not create " + type + " " + id + ". That id is already in use", cause);
 	}
 
-	public CreationException(String type, String id) {
-		this(type, id, null);
-	}
 }
