2319df03287691dbaf077fc4d82c33f95a01d35c
Zion <zionman@verizon.net>
2021-10-22 17:05:18 -0500

    credential env variable

--- product-service/src/main/java/com/cts/capstone/Main.java
+++ product-service/src/main/java/com/cts/capstone/Main.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-//@EnableEurekaClient
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
