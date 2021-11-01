dc8040bce7aa59d3e6968bf3855ae0020c1fce38
Zion <zionman@verizon.net>
2021-11-01 16:47:28 -0500

    add auth0 sub to customer using as ID to map between auth ID and DB customer ID

--- src/main/java/com/cts/capstone/controller/CustomerController.java
+++ src/main/java/com/cts/capstone/controller/CustomerController.java
@@ -41,6 +41,17 @@ public class CustomerController {
 		return find;
 	}
 
+	@GetMapping("sub")
+	public Customer getCustomerBySub(@RequestBody String sub) {
+		Customer find = customerService.findBySub(sub);
+		System.out.println(find);
+		System.out.println(sub);
+		if (find == null) {
+			throw new CustomerNotFoundException();
+		}
+		return find;
+	}
+
 	@PostMapping()
 	public ResponseEntity<Customer> addCustomer(@Valid @RequestBody Customer customer) {
 		Customer added = customerService.add(customer);
