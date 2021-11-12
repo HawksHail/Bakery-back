e50285a75e8570d735019fa9f65626adc776de02
Zion <zionman@verizon.net>
2021-11-12 14:56:58 -0600

    Added logging to controllers

---
+++ src/main/java/com/cts/capstone/config/RequestLoggingInterceptor.java
@@ -0,0 +1,33 @@
+package com.cts.capstone.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			Method method = handlerMethod.getMethod();
+			LOGGER.info("{} - {} - {} - method '{}' on controller '{}' - status {}",
+					request.getMethod(),
+					request.getRequestURI(),
+					request.getRemoteAddr(),
+					method.getName(),
+					handlerMethod.getBean().getClass(),
+					response.getStatus());
+		}
+
+	}
+}
---
+++ src/main/java/com/cts/capstone/config/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.cts.capstone.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new RequestLoggingInterceptor());
+	}
+}
